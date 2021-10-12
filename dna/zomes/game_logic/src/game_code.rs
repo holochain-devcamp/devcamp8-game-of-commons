d0dd6425c91b7c03a88289622de0246fa0ee4a79
e-nastasia <32966301+e-nastasia@users.noreply.github.com>
2021-10-12 12:58:40 +0300

    rustfmt

--- dna/zomes/game_logic/src/game_code.rs
+++ dna/zomes/game_logic/src/game_code.rs
@@ -37,10 +37,10 @@ pub fn get_game_code_anchor(game_code: String) -> ExternResult<EntryHash> {
     /* Since do not know the hash of the anchor, because only the game code is known,
     we have to calculate the hash.
     */
-        let path: Path = (&Anchor {
-            anchor_type: GAME_CODES_ANCHOR.into(),
-            anchor_text: Some(game_code),
-        })
-            .into();
-        path.hash()
+    let path: Path = (&Anchor {
+        anchor_type: GAME_CODES_ANCHOR.into(),
+        anchor_text: Some(game_code),
+    })
+        .into();
+    path.hash()
 }
