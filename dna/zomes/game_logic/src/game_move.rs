73ec0432ee6c6cc10fca8e6c21b95001035e3025
e-nastasia <32966301+e-nastasia@users.noreply.github.com>
2021-10-26 14:55:54 +0400

    Add finalize_moves

--- dna/zomes/game_logic/src/game_move.rs
+++ dna/zomes/game_logic/src/game_move.rs
@@ -1,5 +1,6 @@
 use crate::{game_session::ResourceAmount, utils::try_get_and_convert};
 use hdk::prelude::*;
+use std::collections::BTreeMap;
 
 pub const GAME_MOVE_LINK_TAG: &str = "GAME_MOVE";
 
@@ -64,4 +65,52 @@ pub fn get_moves_for_round(last_round_hash: EntryHash) -> ExternResult<Vec<GameM
         moves.push(game_move);
     }
     Ok(moves)
+}
+
+/// Consumes list of moves passed to it to finalize them.
+/// If every player made at least one move, it returns list of moves which is guaranteed
+/// to have a single move for every player.
+/// If there are missing moves, it returns None, since we can't finalize the moves and
+/// have to wait for other players instead.
+pub fn finalize_moves(
+    moves: Vec<GameMove>,
+    number_of_players: usize,
+) -> ExternResult<Option<Vec<GameMove>>> {
+    // Check that at least we have as many moves
+    // as there are players in the game
+    if moves.len() < number_of_players {
+        info!(
+            "Cannot finalize moves: there are {} players total but only {} moves have been made",
+            number_of_players,
+            moves.len()
+        );
+        return Ok(None);
+    } else {
+        // Now that we know we have moves >= num of players, we need
+        // to make sure that every player made at least one move, so
+        // we're not closing the round without someone's move
+        let mut moves_per_player: BTreeMap<AgentPubKey, Vec<GameMove>> = BTreeMap::new();
+        for m in moves {
+            match moves_per_player.get_mut(&m.owner) {
+                Some(moves) => moves.push(m),
+                // TODO(e-nastasia): cloning owner value seems like a waste, but I think
+                // that alternative would be to use lifetimes. Not sure it's worth the
+                // readability penalty that we'll incur.
+                None => {
+                    moves_per_player.insert(m.owner.clone(), vec![m]);
+                }
+            }
+        }
+        if moves_per_player.keys().len() < number_of_players {
+            info!("Cannot close the round: only {} players made their moves, waiting for total {} players", moves_per_player.keys().len(), number_of_players);
+            return Ok(None);
+        }
+        let mut new_moves = vec![];
+        for (_, move_vec) in moves_per_player {
+            // NOTE(e-nastasia): if we add a timestamp to the game move, we'll be able to
+            // filter moves here, but for now we'll do with just taking some move
+            new_moves.push(move_vec[0].clone());
+        }
+        Ok(Some(new_moves))
+    }
 }
